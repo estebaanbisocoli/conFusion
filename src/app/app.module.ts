d8b55d9a981e3bbce159b4c5ebb4d4211fe54b3a
estebaanbisocoli <estebaanbisocoli@gmail.com>
2017-10-09 03:00:08 -0300

    primer envio, no tine el html del componente, instalados los paquetes de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +18,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,  MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
